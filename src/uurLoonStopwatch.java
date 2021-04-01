25eaed7c04b94596ee86254e2eec7c2b47dc32d2
RinseWillet <r.willet@gmail.com>
2021-04-01 15:48:03 +0200

    stap 5

--- src/uurLoonStopwatch.java
+++ src/uurLoonStopwatch.java
@@ -3,27 +3,62 @@ import javax.swing.*;
 
 public class uurLoonStopwatch {
 
-//***********        Stap 4 In deze stap willen we Swing gaan opbouwen
-//        - Uitleggen dat Swing een gedateerde API van Oracle is die je in staat stelt een
-//    Graphical User Interface te maken voor je Java programma. Iets moderner is JavaFX
-//            (maar dat wordt niet ondersteund nog door Repl.It en vereist wat installeerwerk op je IDE)
-//        - Veel moderner is het je Java programma aan een frontend in JS, html, Css te hangen, maar dat kost teveel tijd voor hier
-//        - beginnen met het opzetten van een frame
-//     
-//		----------------------------------
+//************ Stap 5 Knop gaan bouwen om een tijdsmeting te doen
+//		------------------------------------
+//    JButton knop=new JButton("Tijd opnemen");
+//		knop.setBounds(30,100,120, 40);
+//
+//		kader.add(knop)
+//
+//            ------------------------------------
+//    en dan een eventlistener toevoegen aan de knop (zodat ie ook wat gaat doen zodra er geklikt wordt
+//            ---------------------------------
+//            knop.addActionListener(e ->
+//    {
+//        System.out.println("knop gedrukt");
+//    });
+//		-----------------------------------
+//    en dan moeten we gebruik gaan maken van de tijd en daar een meting van maken
+//		-----------------------------------
+//    klok.eind = LocalDateTime.now();
+//    Duration tijdsVerschil = Duration.between(klok.start, klok.eind);
+//        System.out.println("Het verschil is : " + tijdsVerschil);
+//        -------------------------------------
+//    je ziet dat de tijd blijft oplopen en het verschil groter wordt, en dat komt doordat het beginpunt (klok.start)
+//    nog steeds hetzelfde blijft. We zullen die dus moeten herïnitialiseren:
+//            -----------------------------------
+//    klok.start = klok.eind;
+//        ----------------------------------
+//    we hebben nu dus een knop die een tijdsmeting start en de periode meet tussen dat je op de knop gedrukt hebt
+
 
     public static void main (String[] args){
-        JFrame kader = new JFrame();
 
         tijdsMeting klok = new tijdsMeting();
+
+        JFrame kader = new JFrame();
+
+        JButton knop=new JButton("Tijd opnemen");
+        knop.setBounds(30,100,120, 40);
+
+        knop.addActionListener(e -> {
+        System.out.println("knop gedrukt");
+        klok.eind = LocalDateTime.now();
+        Duration tijdsVerschil = Duration.between(klok.start, klok.eind);
+        System.out.println("Het verschil is : " + tijdsVerschil);
+        klok.start = klok.eind;
+        });
+
+
         System.out.println(klok.start);
         System.out.println(klok.eind);
 
+        kader.add(knop);
         kader.setSize(1000,600);
         kader.setLayout(null);
         kader.setVisible(true);
 
-
+///SCRIBBLES
 //        System.out.println("Klaar voor de start");
 //        LocalTime tijdStip1 = LocalTime.of(8,5,10);
 //        LocalTime tijdStip2 = LocalTime.of(19,3,00);
