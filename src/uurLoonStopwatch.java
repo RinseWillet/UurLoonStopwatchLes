1ab3d918a863b9dad9375b2a98dbf152d83bdf40
RinseWillet <r.willet@gmail.com>
2021-04-01 15:58:33 +0200

    stap 6

--- src/uurLoonStopwatch.java
+++ src/uurLoonStopwatch.java
@@ -3,33 +3,49 @@ import javax.swing.*;
 
 public class uurLoonStopwatch {
 
-//************ Stap 5 Knop gaan bouwen om een tijdsmeting te doen
-//		------------------------------------
-//    JButton knop=new JButton("Tijd opnemen");
-//		knop.setBounds(30,100,120, 40);
+//*********** Stap 6 Velden bouwen om de Activiteit en het Uurloon in te vullen
+//    We gaan nu gebruik maken van een Swing Textfields om een veld te maken voor:
+//      * de activiteit
+//		* het uurloon
+//		* een outputveld waar de taken komen te staan
+//		* ook geven we alle velden labels
+//		----------------------------------------
+//    JTextField activiteitVeld = new JTextField();
+//		activiteitVeld.setBounds(30,150,100, 40);
 //
-//		kader.add(knop)
+//		kader.add(activiteitVeld);
+//		------------------------------------------
+//    nu draaien en dan zien we het veld
+//		------------------------------------------
+//    JTextField uurLoon = new JTextField(10);
+//		uurLoon.setBounds(30,200,100, 40);
 //
-//            ------------------------------------
-//    en dan een eventlistener toevoegen aan de knop (zodat ie ook wat gaat doen zodra er geklikt wordt
-//            ---------------------------------
-//            knop.addActionListener(e ->
-//    {
-//        System.out.println("knop gedrukt");
-//    });
-//		-----------------------------------
-//    en dan moeten we gebruik gaan maken van de tijd en daar een meting van maken
-//		-----------------------------------
-//    klok.eind = LocalDateTime.now();
-//    Duration tijdsVerschil = Duration.between(klok.start, klok.eind);
-//        System.out.println("Het verschil is : " + tijdsVerschil);
-//        -------------------------------------
-//    je ziet dat de tijd blijft oplopen en het verschil groter wordt, en dat komt doordat het beginpunt (klok.start)
-//    nog steeds hetzelfde blijft. We zullen die dus moeten herïnitialiseren:
-//            -----------------------------------
-//    klok.start = klok.eind;
-//        ----------------------------------
-//    we hebben nu dus een knop die een tijdsmeting start en de periode meet tussen dat je op de knop gedrukt hebt
+//		kader.add(uurLoon);
+//		-------------------------------------------
+//    nu draaien en dan zien we het tweede veld eronder.
+//    Nu willen we een tekstblok maken waarin we de lijst maken van de taken en
+//    de hoeveelheid seconden.
+//		-------------------------------------------
+//    JTextArea textArea = new JTextArea(50, 200);
+//		textArea.setBounds(30, 250, 700, 200);
+//
+//		kader.add(textArea);
+//		-------------------------------------------
+//    nu draaien en dan zien we een tekstarea eronder. Nu zouden wat labels nog wel heel handig zijn
+//		----------------------------------------------
+//    JLabel activiteitLabel = new JLabel("Voer hier de activiteit in");
+//		activiteitLabel.setBounds(150,150,200,40);
+//
+//		kader.add(activiteitLabel);
+//		----------------------------------------------
+//    en nu nog voor het uurloon
+//		----------------------------------------------
+//    JLabel uurLoonLabel = new JLabel("Voer hier het uurloon in met een . ");
+//		uurLoonLabel.setBounds(150,200,200,40);
+//
+//		kader.add(uurLoonLabel)
+//            ------------------------------------------------
+//    en dan hebben we nu de velden in onze GUI uiteengezet
 
 
     public static void main (String[] args){
@@ -40,6 +56,21 @@ public class uurLoonStopwatch {
 
         JButton knop=new JButton("Tijd opnemen");
         knop.setBounds(30,100,120, 40);
+
+        JTextField activiteitVeld = new JTextField();
+		activiteitVeld.setBounds(30,150,100, 40);
+
+        JTextField uurLoon = new JTextField(10);
+		uurLoon.setBounds(30,200,100, 40);
+
+        JTextArea lijst = new JTextArea(50, 200);
+		lijst.setBounds(30, 250, 700, 200);
+
+        JLabel activiteitLabel = new JLabel("Voer hier de activiteit in");
+		activiteitLabel.setBounds(150,150,200,40);
+
+        JLabel uurLoonLabel = new JLabel("Voer hier het uurloon in met een . ");
+    	uurLoonLabel.setBounds(150,200,200,40);
 
         knop.addActionListener(e -> {
         System.out.println("knop gedrukt");
@@ -54,6 +85,11 @@ public class uurLoonStopwatch {
         System.out.println(klok.eind);
 
         kader.add(knop);
+        kader.add(activiteitVeld);
+        kader.add(uurLoon);
+        kader.add(lijst);
+        kader.add(activiteitLabel);
+        kader.add(uurLoonLabel);
         kader.setSize(1000,600);
         kader.setLayout(null);
         kader.setVisible(true);
