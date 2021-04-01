7e847dac97475e09e1ebae8a2f4ab49c693995fe
RinseWillet <r.willet@gmail.com>
2021-04-01 15:28:05 +0200

    stap 1

---
+++ src/uurLoonStopwatch.java
@@ -0,0 +1,16 @@
+
+public class uurLoonStopwatch {
+
+    //Stap 1
+    // Uitleg wat we willen maken
+    //Machine waarin je je taak en je uurloon kunt invullen, zodra je op de knop klikt laat ie zien
+    //hoe lang je met de klus bent bezig geweest, zet het op een lijstje, en houd bij hoeveel je in
+    //totaal hebt verdiend.
+
+    public static void main (String[] args){
+
+        System.out.println("Klaar voor de start");
+
+    }
+
+}
